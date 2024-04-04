a546d269da6d9ab9aaa38b05b4329577a7e592ed
Ryabbb <ryableonard@gmail.com>
2024-04-04 11:34:55 -0400

    Formatted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect } from 'react'; // Import Suspense and lazy here
+import React, { Suspense, lazy, useEffect } from 'react'; 
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,15 +10,15 @@ import {
   Navigate,
 } from 'react-router-dom';
 
-// Utility function to capitalize the first letter of a string
+
 const capitalize = (s) => {
   if (typeof s !== 'string') return '';
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
 
 const DynamicGameComponent = () => {
-  const { gameName } = useParams(); // Using useParams to extract parameters
-  // Dynamically import the game component based on the URL path
+  const { gameName } = useParams(); 
+  
   const GameComponent = lazy(() =>
     import(`./${capitalize(gameName)}/${capitalize(gameName)}`).catch(() =>
       import('./NotFound')
@@ -34,17 +34,17 @@ const DynamicGameComponent = () => {
 
 function App() {
   useEffect(() => {
-    // Function to prevent default action for up and down arrow keys
+    
     const preventScroll = (e) => {
       if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
         e.preventDefault();
       }
     };
     
-    // Add event listener when component mounts
+    
     window.addEventListener('keydown', preventScroll);
     
-    // Cleanup function to remove event listener when component unmounts
+    
     return () => window.removeEventListener('keydown', preventScroll);
   }, []);
 
